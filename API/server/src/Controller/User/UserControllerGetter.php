c1e0c259bcf1323005faf9d814a581b282a21306
rainald28700 <rainlad4567@gmail.com>
2019-10-28 21:25:16 +0100

    Router: user get name

--- API/server/src/Controller/User/UserControllerGetter.php
+++ API/server/src/Controller/User/UserControllerGetter.php
@@ -4,7 +4,32 @@
 namespace App\Controller\User;
 
 
-class UserControllerGetter
-{
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class UserControllerGetter
+ * @package App\Controller\User
+ * @Route("/user", name="app_user", methods={"GET"})
+ */
+
+class UserControllerGetter extends AbstractController
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->em = $entityManager;
+    }
+
+    /**
+     * @Route("/name", name="app_user_name", methods={"GET"})
+     */
+    function getUserName(){
+        $query = $this->em->createQuery('
+                  SELECT u.name from App\Entity\User u ');
+        $result = $query->execute();
+        return $this->json($result);
+    }
 }
