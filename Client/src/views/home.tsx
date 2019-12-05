f0f13fa2c7138d7f0dbfc464ab8f13b5c8a10720
quentinaime <quentin.aime.dev@gmail.com>
2019-12-05 10:39:33 +0100

    fix

--- Client/src/views/home.tsx
+++ Client/src/views/home.tsx
@@ -2,23 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 
-export const Home = () => {
-  return (
-    <>
-      <h1>This is a home</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li>
-            <Link to="/list">Liste</Link>
-          </li>
-        </ul>
-      </nav>
-    </>
-  )
-}
+export const Home = () => (
+  <>
+    <h1>This is a home</h1>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/list">Liste</Link>
+        </li>
+      </ul>
+    </nav>
+  </>
+)
